9e0a383ad8aef94ed203b8d047949525e08ca9d9
Natalia Matias <nataliamps@outlook.com>
2025-12-09 13:47:12 -0300

    Att arquivos Server e Utils

--- src/server.c
+++ src/server.c
@@ -6,32 +6,31 @@
 /*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 15:22:51 by namatias          #+#    #+#             */
-/*   Updated: 2025/12/07 10:35:01 by namatias         ###   ########.fr       */
+/*   Updated: 2025/12/09 13:13:41 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void signal_handler(int signum)
+int	main(void)
 {
-   
-    if (g_bit_count == 8)
-    {
-        printf("%c", g_char);//TODO: imprimir o char recebido ate agora
-    }
-}
-int main(void)
-{
-    struct sigaction sa;
-    
-    sa.sa_handler = signal_handler; //TODO
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-    
-    while (1)
-        pause();      
-    return 0;
+	struct sigaction	s_sigaction;
+	pid_t				pid;
+
+	pid = getpid();
+
+	write(1, "Process ID (PID): ", 18);
+	ft_putnbr(pid);
+	write(1, "\n", 1);
+
+	sigemptyset(&s_sigaction.sa_mask);
+	s_sigaction.sa_sigaction = signal_handler; //TODO: Fazer como o sinal sera tratado. 
+	s_sigaction.sa_flags = SA_SIGINFO;
+	
+	sigaction(SIGUSR1, &s_sigaction, NULL);
+	sigaction(SIGUSR2, &s_sigaction, NULL);
+
+	while (1)
+		pause();
+	return (0);
 }

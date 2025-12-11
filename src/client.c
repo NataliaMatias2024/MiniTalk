396ae0891d8b068d8474f900d6807017e3cfcd99
Natalia <namatias@42sp.org.br>
2025-12-11 12:53:07 -0300

    codigos explicados

--- src/client.c
+++ src/client.c
@@ -3,20 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/07 10:06:47 by namatias          #+#    #+#             */
-/*   Updated: 2025/12/10 17:55:20 by namatias         ###   ########.fr       */
+/*   Updated: 2025/12/11 12:51:06 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-/*
-** Global variable used to synchronize the client with the server.
-** It is updated inside the signal handler when an signal is received.
-** the client only sends the next bit after the server's confirmation.
-*/
+// static restringe o escopo da variável, ou seja, apenas o client.c terá acesso
+// volatile avisa pro compilador nao otimizar essa variavel!! 
+//Assim ele nao acusa que loopings como while (signal_flag == 0) de ser sempre zero
+//e tente substituir por while (1), gerando um looping infinito
+//Alem disso a função principal da nossa flag ficar salvo em uma variavel global é
+//que usamos funções diferentes que utilizam dela para funcionar, e nao podemos
+//redefini-la dentro das funções pois isso faria com que o valor nao fosse compartilhado
+//Assim o papel da global é avisar ao compilador:
+//A variavel signal_flag utilizada nas funções set_signal_flag e signal_sender são as MESMA!
+//E estao definidar aqui com essas caracteristicas! 
 static volatile int	signal_flag = 0;
 
 static void	set_signal_flag(int signal)
@@ -28,16 +33,22 @@ static void	set_signal_flag(int signal)
 void	signal_sender(pid_t server_pid, unsigned char c)
 {
 	int	i;
-
+//Nossa função receve o pid do server e um char = 1 byte = 8 bits
+//por isso nosso looping será repetido 8 vezes, uma para cada bit
 	i = 0;
 	while (i < 8)
 	{
+		//começamos setando a signal_flag para 0, pois nesse momento vamos enviar nossas infos
 		signal_flag = 0;
+		//Deslocamos o bit da posição i para a direita, isolamos ele com o & e vemos seu valor.
+		//SE for igual a 1 enviamos SIGURS1 caso contrario enviamos o SIGURS2
 		if (((c >> i) & 1) == 1)
 			kill(server_pid, SIGUSR1);
 		else
 			kill(server_pid, SIGUSR2);
 		i++;
+		//Apos cada envio nosso client fica em espera ate receber o sinal 1 de volta do servidor. 
+		//Só depois ela envia o proximo bit
 		while (signal_flag == 0)
 			pause();
 	}
@@ -49,22 +60,36 @@ int	main(int argc, char **argv)
 	pid_t				server_pid;
 	int					i;
 
+//Verifico se estamos recebendo o numero de argumentos validos (programa + pid server + mensagem a ser enviada)
 	if (argc != 3)
 		return (1);
+//Para o pid ser recebido no formato correto realizamos um atoi e fazemos um cast para o formato exigido para o envio de sinal (pid_t)
 	server_pid = (pid_t)ft_atoi(argv[1]);
+//Com o pod salvo, verificamos se foi passado um numero invalido (negativo) OU se o envio de teste kill(pid , 0) retorna erro (-1).
+//Assim podemos testar se o pid é valido e se temos autorização para enviar sinais para ele SEM de fato enviar.
 	if (server_pid <= 0 || kill(server_pid, 0) == -1)
 		return (1);
+//Essa função server para limpar as configurações pré existentes/ lixos de memoria. Evitando comportamentos inesperados
 	sigemptyset(&s_sigaction.sa_mask);
+//Avisamos que vamos usar o modo padrão/mais simpes de envio de sinal
 	s_sigaction.sa_flags = 0;
+//Informamos como iremos reagir ao sinal do servidor, ou seja, quando receber um aviso do servidor chamaremos direto a função set_signal_flag
+//Ela nos avisará que podemos enviar o proximo char
 	s_sigaction.sa_handler = set_signal_flag;
+//Realizamos o cadastro das configurações estabelecidas, informando que receberemos o SIGUSR1 do server e acionaremos a funçao set_signal_flag
 	if (sigaction(SIGUSR1, &s_sigaction, NULL) == -1)
 		return (1);
+//Após as configurações iniciais, começamos o envio da mensagem de fato!
+//O argv[2] é a mensagem inteira, por isso repetiremos o looping até a ultima posição
 	i = 0;
 	while (argv[2][i])
 	{
+		//para cada i enviado, ou seja, para cada caracter enviado utilizaremos a função signal_sender
+		//ela que transformará o char em bits e enviará bit a bit para nosso server. 
 		signal_sender(server_pid, (unsigned char)argv[2][i]);
 		i++;
 	}
+	//como nosso looping se encerra antes de enviar o operador de fim de string, enviamos ele separadamente fora do looping
 	signal_sender(server_pid, '\0');
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: namatias <namatias@42sp.org.br>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 15:22:51 by namatias          #+#    #+#             */
-/*   Updated: 2025/12/10 18:05:49 by namatias         ###   ########.fr       */
+/*   Updated: 2025/12/11 12:51:55 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,25 @@ void	signal_handler(int signal, siginfo_t *infos_sigaction, void *empty)
 	static t_signal		config;
 
 	(void) empty;
+	//Primeiro vamos guardar o ID do client que esta enviando a mensagem. 
+	//Nosso server precisa receber sinais de diferentes clientes SEM misturar os bits recebidos.
+	//Se for um cliente novo o campo client_pid estará zerado e entrará no if
 	if (config.client_pid != infos_sigaction->si_pid)
 	{
+		//Resetamos todos os campos para que nao seja pego lixo de memoria da leitura anterior
 		config.bits_received = 0;
 		config.current_char = 0;
+		//Salvamos o pid do nomo CLIENT
 		config.client_pid = infos_sigaction->si_pid;
 	}
+	//Verificamos o sinal que estamos recebendo. SE for 1 iremos utilizar o operador OU de bit
+	//assim garantimos q ele seja adicionado sem alterar nenhum outro valor que ja estiver la
 	if (signal == SIGUSR1)
 		config.current_char |= (1 << config.bits_received);
+	//SE o sinal recebido for 0, nós apenas aumentamos o contador pois o zero é preenchido automaticamente
 	config.bits_received++;
+	//Quando tivermos os 8bits podemos finalmente imprimir o char, e resetar os contadores para o
+	//proximo char a ser impresso. 
 	if (config.bits_received == 8)
 	{
 		if (config.current_char == '\0')
@@ -35,6 +45,8 @@ void	signal_handler(int signal, siginfo_t *infos_sigaction, void *empty)
 		config.bits_received = 0;
 		config.current_char = 0;
 	}
+	//Finalmente enviamos um sinal para o client avisando que ja recebemos os 8bits e imprimimos o char
+	//que ele pode mandar o proximo.
 	if (infos_sigaction->si_pid > 0)
 		kill(infos_sigaction->si_pid, SIGUSR1);
 }
